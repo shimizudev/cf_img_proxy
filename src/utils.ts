f7abbb20f1b5f19298d5eedb5996e641a0a6fe9d
Mohtasim Alam Sohom <94981761+shimizudev@users.noreply.github.com>
2025-01-18 21:08:16 +0600

    hahahahahahhahaha

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,7 @@
 export function hexDecode(hex: string): string {
-    return Buffer.from(hex, 'hex').toString();
+    const bytes = new Uint8Array(hex.length / 2);
+    for (let i = 0; i < hex.length; i += 2) {
+        bytes[i / 2] = parseInt(hex.slice(i, i + 2), 16);
+    }
+    return new TextDecoder().decode(bytes);
 }
